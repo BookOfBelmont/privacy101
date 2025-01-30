7e63cd5ed93674b3002a9c8a83eb6fc521fbc8ce
Harley Jonatan <harleyjonatan@trentu.ca>
2025-01-29 22:50:32 -0500

    testing cryptpad sizing

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!!!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <iframe src="https://cryptpad.fr/code/#/2/code/view/qXCjFOMYKmJVkFBDaHh+DJqNiNzLfOwRNcR+b0OnMt4/p/embed/present/"></iframe>
-      </header>
+      <iframe src="https://cryptpad.fr/code/#/2/code/view/qXCjFOMYKmJVkFBDaHh+DJqNiNzLfOwRNcR+b0OnMt4/p/embed/present/"></iframe>
     </div>
   );
 }
