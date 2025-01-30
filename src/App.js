719f84452cd38ebbe77cf574b3d7e3111fa9da14
M B <null_email>
2025-01-29 23:50:44 -0500

    fixed spacing issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+let page = 'landing-page';
+let MainApp;
+
+if (page === 'landing-page') {
+  MainApp = () => {
+    return <iframe src="https://cryptpad.fr/code/#/2/code/view/qXCjFOMYKmJVkFBDaHh+DJqNiNzLfOwRNcR+b0OnMt4/p/embed/present/"></iframe>
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <iframe src="https://cryptpad.fr/code/#/2/code/view/qXCjFOMYKmJVkFBDaHh+DJqNiNzLfOwRNcR+b0OnMt4/p/embed/present/"></iframe>
+    <div className='App'>
+      <MainApp className="app" />
     </div>
   );
 }
