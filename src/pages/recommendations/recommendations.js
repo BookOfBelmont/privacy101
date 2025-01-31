21496db2af8809508f9d7eb8d7ffb70f6a91e26a
Harley Jonatan <harleyjonatan@trentu.ca>
2025-01-30 21:33:30 -0500

    internall links working. added blank homepage

---
+++ src/pages/recommendations/recommendations.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../styles/recommendations.css';
+
+function Recommendations() {
+    return (
+        <div>
+            <h1>recommendations!!!!!</h1>
+        </div>
+    );
+}
+
+export default Recommendations;
