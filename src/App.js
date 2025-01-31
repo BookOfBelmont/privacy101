f24abd902a249f911096e2815faa06661567bbb6
M B <null_email>
2025-01-30 20:03:34 -0500

    landing page correct embed link

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
 import Title from './components/title';
 
-let page = 'landing-page';
+let page = 1;
 let MainApp;
 
-if (page === 'landing-page') {
+if (page === 1) {
   MainApp = () => {
-    return <iframe src="https://cryptpad.fr/code/#/2/code/view/qXCjFOMYKmJVkFBDaHh+DJqNiNzLfOwRNcR+b0OnMt4/p/embed/present/"></iframe>
+    return <iframe src="https://cryptpad.fr/code/#/2/code/view/PrKlfyiodl35f6mEzpN6MbNyEg8+QzxFFJ2WqJkoGks/p/embed/present/"></iframe>
   }
+} else if (page === 2) {
+  MainApp = () => {
+    return <h1>page 2 || learning hub</h1>
+  }
+} else if (page === 3) {
+    MainApp = () => {
+      return <h1>page 3 || recommendations</h1>
+    }
 }
 
 function App() {
   return (
     <div className='App'>
       <Title />
-      <MainApp className="app" />
+      <MainApp />
     </div>
   );
 }
