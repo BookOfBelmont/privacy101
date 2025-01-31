801a44ef45e95d3343ab7b15e73bb14abf6905a6
M B <null_email>
2025-01-30 23:06:04 -0500

    linking should work.

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,15 @@
+import Title from "../../global-components/title";
+import "../../styles/homepage.css";
+
+function Homepage() {
+    return (
+        <div>
+            <Title />
+            <div class="homepage">
+                <h1>homepage</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Homepage;
--- src/pages/learning-hub/learning-hub.js
+++ src/pages/learning-hub/learning-hub.js
@@ -1,11 +1,13 @@
 import React from "react";
 import '../../styles/learning-hub.css';
 import './table-of-contents-learning-hub';
+import Title from "../../global-components/title.js";
 import TableOfContents from "./table-of-contents-learning-hub";
 
 function LearningHub() {
     return (
         <div>
+            <Title />
             <TableOfContents />
 
         </div>
