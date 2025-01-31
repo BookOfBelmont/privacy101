a3b6c3b3afb2e060a1a1e563024e7b08f7c7050c
Harley-J <harleyjonatanis@gmail.com>
2025-01-31 18:58:01 -0500

    test3

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -4,7 +4,7 @@ import Title from "../../global-components/title.js";
 
 function LandingPage() {
     return (
-        <div>
+        <div className="landing-page">
             <Title />
             <iframe src="https://cryptpad.fr/code/#/2/code/view/OwytGOUVgNpdEanZb+0XKdyPCg3mFnr8Gp5JqqcAn2Q/embed/present/"></iframe>
         </div>
