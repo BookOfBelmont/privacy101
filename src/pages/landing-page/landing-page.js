275a4ac4e02d922995427af86cd4ae31b048a7ad
Harley-J <harleyjonatanis@gmail.com>
2025-01-31 18:20:23 -0500

    made cryptpad doc public

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -6,7 +6,7 @@ function LandingPage() {
     return (
         <div>
             <Title />
-            <iframe src="https://cryptpad.fr/code/#/2/code/view/PrKlfyiodl35f6mEzpN6MbNyEg8+QzxFFJ2WqJkoGks/p/embed/present/"></iframe>
+            <iframe src="https://cryptpad.fr/code/#/2/code/view/OwytGOUVgNpdEanZb+0XKdyPCg3mFnr8Gp5JqqcAn2Q/embed/present/"></iframe>
         </div>
     );
 }
