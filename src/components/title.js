e22fc5a72e0e95cbf91d3f2b2ea2395c9e4eef18
M B <null_email>
2025-01-30 19:47:16 -0500

    created title component, fixed file structure and created basic styles

---
+++ src/components/title.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/title.css";
+
+function Title() {
+    return (
+        <div>
+            <h1 class="primary-title">
+	            <span>
+                    <span id="redact">T</span><span id="redact">h</span><span id="redact">e&nbsp;</span>
+	            </span>
+	            <span id="singleRedact">
+	        	    <span id="redact">H</span><span id="redact">a</span><span id="redact">r</span><span id="redact">l</span><span id="redact">e</span><span id="redact">y&nbsp;</span> 
+	            </span>
+                <span>      
+                    <span id="redact">P</span><span id="redact">r</span><span id="redact">o</span><span id="redact">t</span><span id="redact">o</span><span id="redact">c</span><span id="redact">o</span><span id="redact">l</span>
+                </span> 
+            </h1>
+            <h1 class="secondary-title" >A guide to privacy and security in the digital world</h1>
+            <br/>
+            <h1 class="secondary-title" >- Landing Page -</h1>
+        </div>
+    );
+}
+
+export default Title;
