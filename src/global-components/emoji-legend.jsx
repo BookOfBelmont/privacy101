e37efea548608a99a2e0c1a046d43d6aeb2131ec
Harley Jonatan <harleyjonatan@trentu.ca>
2025-02-04 04:47:54 -0500

    started learning hub lists. created map-data js.

---
+++ src/global-components/emoji-legend.jsx
@@ -0,0 +1,13 @@
+function EmojiLegend() {
+    return (
+        <div classNameName="emoji-legend">
+            <p className="text">🌱  ={">"}  New</p>
+            <p className="text">⭐️  ={">"}  Excellent source, start here!</p>
+            <p className="text">🏛️  ={">"}  Instructional</p>
+            <p className="text">🚨  ={">"}  Current events</p>
+            <p className="text">🎬  ={">"}  Video</p>
+        </div>
+    );
+}
+
+export default EmojiLegend;
---
+++ src/pages/learning-hub/guide-links/tech-concepts/ip-and-dns-data.js
@@ -0,0 +1,15 @@
+/* tags: ['new', 'exc', 'inst', 'curr', 'vid'] */
+
+export const ip_and_dns_data = [
+    {
+        title: "",
+        url: "",
+        new_item: false,
+        excellent: false,
+        instructional: false,
+        current: false,
+        video: false,
+        tags: [0, 0, 1, 0, 1]
+    },
+]
+
